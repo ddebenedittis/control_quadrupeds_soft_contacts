c81aa517f6a1a0cecacc7e5ac9d2e9a96a6f3f60
ddebenedittis <davide.debenedittis@gmail.com>
2023-03-21 17:50:57 +0100

    RViz submodule added correctly

--- src/rviz_legged/rviz_legged_plugins/rviz_legged_plugins-extras.cmake
+++
@@ -1,3 +0,0 @@
-# find package Qt5 because otherwise using the rviz_default_plugins::rviz_default_plugins
-# exported target will complain that the Qt5::Widgets target does not exist
-find_package(Qt5 REQUIRED QUIET COMPONENTS Widgets)
